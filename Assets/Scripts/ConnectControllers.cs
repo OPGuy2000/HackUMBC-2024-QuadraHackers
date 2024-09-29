8a3e73bbe5423439a67d343a8b5550e8639b54f2
Varun Sammeta <diamondblazer59@gmail.com>
2024-09-29 03:40:17 -0400

    why so feinious

---
+++ Assets/Scripts/ConnectControllers.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ConnectControllers : MonoBehaviour
+{
+    public TextMeshProUGUI prompt1;
+    public TextMeshProUGUI prompt2;
+    public TextMeshProUGUI prompt3;
+    public TextMeshProUGUI prompt4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Connect()
+    {
+        string[] controllers = Input.GetJoystickNames();
+        prompt1.text = controllers[0];
+        prompt2.text = controllers[1];
+        prompt3.text = controllers[2];
+        prompt4.text = controllers[3];
+
+    }
+}
